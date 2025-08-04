d7106fc3d88005aaad4a43226e0ea73dac81f441
Keerthinarayan25 <keerthinarayan10@gmail.com>
2025-08-05 00:55:36 +0530

    header and footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="border-b border-gray-100 flex justify-between items-center p-4">
+      <div>Made with ❤️ Keerthi narayan</div>
+    </div>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="border-b border-gray-100 flex justify-between items-center p-4">
+      <div>Logo</div>
+
+      <ul className="flex items-center gap-4">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/post">Post</Link></li>
+
+      </ul>
+    </div>
+  );
+}
