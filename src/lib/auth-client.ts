b8af0fe2f10d3b8b299bbf434b8a2caa0218601d
Keerthinarayan25 <keerthinarayan10@gmail.com>
2025-08-24 19:26:46 +0530

    fixed error

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient} from "better-auth/react"
 
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BASE_URL || "/"
+  baseURL: process.env.BASE_URL || ""
 })
 
 export const { signUp, signIn, signOut, useSession} = authClient;
