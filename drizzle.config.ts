00baa7d5f2db1d85379661716559cd252859f90c
Keerthinarayan25 <keerthinarayan10@gmail.com>
2025-08-24 18:41:56 +0530

    switched to Neon

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out:'./drizzle',
   dialect:'postgresql',
   dbCredentials:{
-    url: process.env.DATABASE_URL || '',
+    url: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_l2UwVOLCf0mF@ep-silent-snow-a1hua064-pooler.ap-southeast-1.aws.neon.tech/blog?sslmode=require&channel_binding=require',
   },
   verbose:true,
   strict: true
